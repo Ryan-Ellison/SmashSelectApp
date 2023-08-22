1365fcdc7790f19015d22fa9333694f358383169
Ryan-Ellison <Ryan.Ellison477@gmail.com>
2023-08-22 15:18:27 -0400

    Initial Commit

---
+++ SmashApp/SmashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmashAppApp.swift
+//  SmashApp
+//
+//  Created by Ryan Ellison on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SmashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
